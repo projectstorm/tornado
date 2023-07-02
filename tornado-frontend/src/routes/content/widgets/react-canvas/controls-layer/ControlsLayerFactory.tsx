4f71a488f5f361310e76001db4cc3ca03bf6e7df
Dylan Vorster <dylanvorster.dv@gmail.com>
2023-07-02 15:07:12 -0600

    selection boxes are now reverse scaled as the canvas zooms

---
+++ tornado-frontend/src/routes/content/widgets/react-canvas/controls-layer/ControlsLayerFactory.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import {
+  AbstractReactFactory,
+  FactoryBank,
+  GenerateModelEvent,
+  GenerateWidgetEvent,
+  LayerModel,
+  LayerModelGenerics
+} from '@projectstorm/react-canvas-core';
+import { ImageElement, ImageElementFactory } from '../image-element/ImageElementFactory';
+import * as _ from 'lodash';
+import { ConceptCanvasEngine } from '../ConceptCanvasEngine';
+import { ConceptCanvasModel } from '../ConceptCanvasModel';
+import { ControlsElementWidget } from './ControlsElementWidget';
+
+export class ControlsLayerFactory extends AbstractReactFactory<ControlsLayerModel, ConceptCanvasEngine> {
+  static TYPE = 'concept/controls/layer';
+
+  constructor() {
+    super(ControlsLayerFactory.TYPE);
+  }
+
+  generateModel(event: GenerateModelEvent): ControlsLayerModel {
+    return new ControlsLayerModel();
+  }
+
+  generateReactWidget(event: GenerateWidgetEvent<ControlsLayerModel>): JSX.Element {
+    return (
+      <>
+        {_.map(event.model.getParent().getLayers()[0].getModels(), (m: ImageElement) => {
+          return <ControlsElementWidget model={m} key={m.getID()} />;
+        })}
+      </>
+    );
+  }
+}
+
+export type ControlsLayerGenerics = LayerModelGenerics & {
+  ENGINE: ConceptCanvasEngine;
+  PARENT: ConceptCanvasModel;
+};
+
+export class ControlsLayerModel extends LayerModel<ControlsLayerGenerics> {
+  constructor() {
+    super({
+      type: ControlsLayerFactory.TYPE,
+      transformed: false
+    });
+  }
+
+  getChildModelFactoryBank(engine: ControlsLayerGenerics['ENGINE']): FactoryBank<ImageElementFactory> {
+    return engine.elementBank;
+  }
+}
